92ff3ae11cfff2bf6ecb48722880701fa294c785
tnakamura0314 <Reggae.a0215@gmail.com>
2021-08-18 19:41:55 +0900

    投稿とコメントの削除機能の実装

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -25,78 +25,96 @@ import com.example.repository.CommentRepository;
 @Controller
 @RequestMapping("/Article")
 public class ArticleController {
-	
+
 	@Autowired
 	private ArticleRepository articleRepository;
-	
+
 	@Autowired
-	private CommentRepository  commentRepository;
-	
+	private CommentRepository commentRepository;
+
 	@ModelAttribute
 	public ArticleForm setUpArticleForm() {
 		return new ArticleForm();
 	}
-	
+
 	@ModelAttribute
 	public CommentForm setUpCommentForm() {
 		return new CommentForm();
 	}
-	
+
 	/**
 	 * 記事を一覧表示する.
 	 * 
 	 * @param model requestスコープ
-	 * @return　記事一覧
+	 * @return 記事一覧
 	 */
 	@RequestMapping("")
 	public String index(Model model) {
-		
+
 		List<Article> articleList = articleRepository.findAll();
-		
-		for(int i = 0 ; i < articleList.size() ; i++) {
-		  Article article = articleList.get(i);
-		  List<Comment> commentList = commentRepository.findByArticleId(article.getId());
-		  article.setCommentList(commentList);
+
+		for (int i = 0; i < articleList.size(); i++) {
+			Article article = articleList.get(i);
+			List<Comment> commentList = commentRepository.findByArticleId(article.getId());
+			article.setCommentList(commentList);
 		}
-		
+
 		model.addAttribute("articleList", articleList);
-		
+
 		return "article-comment";
 	}
-	
+
 	/**
 	 * 記事を投稿する.
 	 * 
-	 * @param articleForm　フォームクラス
-	 * @param model　requestスコープ
-	 * @return　記事投稿
+	 * @param articleForm フォームクラス
+	 * @param model       requestスコープ
+	 * @return 記事投稿
 	 */
 	@RequestMapping("/insertArticle")
 	public String insertArticle(ArticleForm articleForm, Model model) {
-		
+
 		Article article = new Article();
 		BeanUtils.copyProperties(articleForm, article);
 		articleRepository.insert(article);
-		
+
 		return "redirect:/Article";
-		
+
 	}
-	
+
 	/**
 	 * コメントを投稿する.
 	 * 
-	 * @return　コメント投稿
+	 * @return コメント投稿
 	 */
 	@RequestMapping("/insertComment")
 	public String insertComment(CommentForm commnetForm, Model model) {
-		
+
 		Comment comment = new Comment();
 		BeanUtils.copyProperties(commnetForm, comment);
 		commentRepository.insert(comment);
-		
+
 		return "redirect:/Article";
-		
+
 	}
-	
+
+	/**
+	 * 記事とコメントを削除する.
+	 * 
+	 * @param articleId 投稿ID
+	 * @return　投稿とコメントを削除
+	 */
+	@RequestMapping("deleteArticle")
+	public String deleteArticle(Integer articleId) {
+
+		commentRepository.deleteByArticleId(articleId);
+		
+		Article article = new Article();
+		article.setId(articleId);
+		articleRepository.deleteById(article.getId());
+
+		return "redirect:/Article";
+
+	}
 
 }
