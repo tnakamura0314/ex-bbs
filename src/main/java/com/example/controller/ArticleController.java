aafa610666d4c73798fab973bde76aade3b8b67f
tnakamura0314 <Reggae.a0215@gmail.com>
2021-08-18 13:54:20 +0900

    ArticleController.javaの作成

---
+++ src/main/java/com/example/controller/ArticleController.java
@@ -0,0 +1,52 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Article;
+import com.example.form.ArticleForm;
+import com.example.repository.ArticleRepository;
+
+/**
+ * 記事情報を操作するコントローラー.
+ * 
+ * @author nakamuratomoya
+ *
+ */
+@Controller
+@RequestMapping("/Article")
+public class ArticleController {
+	
+	@Autowired
+	private ArticleRepository repository;
+	
+	@ModelAttribute
+	public ArticleForm setUpForm() {
+		return new ArticleForm();
+	}
+	
+	/**
+	 * 記事を一覧表示する.
+	 * 
+	 * @param model requestスコープ
+	 * @return　記事一覧
+	 */
+	@RequestMapping("")
+	public String index(ArticleForm articleForm, Model model) {
+		
+		List<Article> articleList = repository.findAll();
+		BeanUtils.copyProperties(articleForm, articleList);
+		
+		model.addAttribute("article", articleList);
+		
+		return "article-commnet";
+	}
+	
+
+}
